d78b001383e3f8554058f03f423d409800b2f006
Omer <bscs18062@itu.edu.pk>
2024-01-31 13:39:18 +0500

    eleventh commit

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     experimentalSessionAndOrigin: true,
     screenshotOnRunFailure:true,
     video: false,
-    videoUploadOnPasses : false
+    videoUploadOnPasses : true
   },
 });
 
