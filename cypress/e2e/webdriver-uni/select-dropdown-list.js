9aa99b8a7c30e0eba5944830ec6855446fcd04a0
Omer <bscs18062@itu.edu.pk>
2024-02-04 01:42:24 +0500

    dropdown list

--- cypress/e2e/webdriver-uni/select-dropdown-list.js
+++ cypress/e2e/webdriver-uni/select-dropdown-list.js
@@ -7,6 +7,18 @@ describe("Interaction with dropdown list via webdriveruni", () => {
         cy.visit("https://www.webdriveruniversity.com")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true })  // overcome issue of multiple browser tabs
 
+        cy.get("#dropdowm-menu-1").select('c#')
+        cy.get("#dropdowm-menu-2").select('testng').should('have.value','testng')
+        cy.get("#dropdowm-menu-3").select('jquery').contains('JQuery')
+        
+        
+        cy.get("#dropdowm-menu-2").select('maven').should('have.value','maven') //asertion based on value
+        cy.get("#dropdowm-menu-2").select('testng').contains('TestNG')           // assertion based on text
+
+
+        cy.get("#fruit-selects").select('apple').contains("Apple")
+        cy.get("#fruit-selects").select('apple').should('have.value','apple')
+        
     })
 
 })
